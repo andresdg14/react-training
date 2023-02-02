7f01365d0fb0d2701e1dcc0cb2fd3d79a4346f93
Andres Dominguez <andresdomlez@gmail.com>
2023-02-02 23:50:29 +0000

    comps project

---
+++ comps/src/App.js
@@ -0,0 +1,46 @@
+import { GoAlert, GoCircleSlash, GoCloudDownload } from 'react-icons/go';
+
+import Button from './Button';
+
+function App() {
+  const handleClick = () => {
+    console.log('Click!!');
+  };
+
+  return (
+    <div>
+      <div>
+        <Button success rounded outline className="my-5" onClick={handleClick}>
+          <GoCloudDownload />
+          Button 1
+        </Button>
+      </div>
+      <div>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCircleSlash />
+          Button 2
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseLeave={handleClick}>
+          <GoAlert />
+          Button 3
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline onMouseHover={handleClick}>
+          <GoCloudDownload />
+          Button 4
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          <GoAlert />
+          Button 5
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
