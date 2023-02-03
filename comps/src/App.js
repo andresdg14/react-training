164c827936936a6d9821bc26996caf490f789bb9
Andres Dominguez <andresdomlez@gmail.com>
2023-02-03 23:44:35 +0000

    Accordion component and conditional rendering

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,46 +1,27 @@
-import { GoAlert, GoCircleSlash, GoCloudDownload } from 'react-icons/go';
-
-import Button from './components/Button';
-
+import Accordion from './components/Accordion';
 function App() {
-  const handleClick = () => {
-    console.log('Click!!');
-  };
+  const items = [
+    {
+      id: 'khj3nlf4',
+      label: 'Can I use React on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+    {
+      id: 'ljkmsfci4',
+      label: 'Can I use Javascript on a project?',
+      content:
+        'You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want.',
+    },
+    {
+      id: 'kjlsdfo83',
+      label: 'Can I use CSS on a project?',
+      content:
+        'You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.',
+    },
+  ];
 
-  return (
-    <div>
-      <div>
-        <Button success rounded outline className="my-5" onClick={handleClick}>
-          <GoCloudDownload />
-          Button 1
-        </Button>
-      </div>
-      <div>
-        <Button danger outline onMouseEnter={handleClick}>
-          <GoCircleSlash />
-          Button 2
-        </Button>
-      </div>
-      <div>
-        <Button warning onMouseLeave={handleClick}>
-          <GoAlert />
-          Button 3
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          <GoCloudDownload />
-          Button 4
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          <GoAlert />
-          Button 5
-        </Button>
-      </div>
-    </div>
-  );
+  return <Accordion items={items} />;
 }
 
 export default App;
