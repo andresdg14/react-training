44e0a781f9f7d256ea2201cad5f17259dfd951e1
Andres Dominguez <andresdomlez@gmail.com>
2023-04-02 21:17:51 +0100

    Deleting an user

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { usersReducer } from './slices/userSlice';
+import { usersReducer } from './slices/usersSlice';
 
 export const store = configureStore({
   reducer: {
@@ -9,3 +9,4 @@ export const store = configureStore({
 
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
+export * from './thunks/removeUser';
